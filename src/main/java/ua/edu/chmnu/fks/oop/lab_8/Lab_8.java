214892a6922240dc9d69c67bdb113d73a7fa5c60
ArslanPars <35731432+ArslanPars@users.noreply.github.com>
2019-05-25 18:15:05 +0300

    Add files via upload

---
+++ src/main/java/ua/edu/chmnu/fks/oop/lab_8/Lab_8.java
@@ -0,0 +1,35 @@
+package ua.edu.chmnu.fks.oop.lab_8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lab_8 {
+
+    public static void main(String args[]) {
+        List<String> list = Arrays.asList("шиншила", "паралеллограм", "синхрофазатрон", "сыр");
+        System.out.println(maxConsonants(list));
+    }
+
+    public static char[] consonants = new char[]{'б', 'в', 'г', 'д', 'ж', 'з', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'};
+
+    public static int consonants(String s) {
+        int counter = 0;
+        char[] charArray = s.toCharArray();
+        for (char ch : charArray) {
+            for (char ch2 : consonants) {
+                if (ch == ch2) {
+                    counter++;
+                    break;
+                }
+            }
+        }
+        return counter;
+    }
+
+    static List<String> maxConsonants(List<String> list) {
+        return list.stream()
+                .filter(c -> consonants(c) > 3)
+                .collect(Collectors.toList());
+    }
+}
